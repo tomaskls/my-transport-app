3c80d31d7dd6a66f581d521017a9ff8cb35399b3
tomaskls <tomaskls@hotmail.com>
2024-10-01 17:04:08 +0300

    en

--- src/i18n.js
+++ src/i18n.js
@@ -8,7 +8,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'LT',
+    fallbackLng: 'EN', 
     supportedLngs: ['EN', 'LT', 'RU'],
     lowerCaseLng: false, 
     debug: true,
